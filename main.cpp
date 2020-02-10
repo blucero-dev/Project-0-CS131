1b558b21d2f0787293111e9228860d0a0bd72629
Brian Lucero <58767149+blucero-dev@users.noreply.github.com>
2020-02-10 08:37:54 -0800

    wip

---
+++ main.cpp
@@ -0,0 +1,30 @@
+// Main Driver file
+// Brian Lucero
+// CS131
+// part 0
+// spring 2020 - started on 2/3/2020
+
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <sstream>
+#include <functional>
+
+
+// Program requirements from pdf :
+// 1. Read a grocery item from standard input (std::cin) until end of file.
+//     For each item read:
+//     a. Store the grocery item in a dynamically allocated object
+//     b. Store the pointer to the grocery item in a standard vector
+
+// 2. After you have reached the end of file, write the grocery items
+//    to standard output (std::cout), in reverse order.
+
+// 3. Be sure to release the dynamically allocated objects before exiting the program
+
+// ============== N O T E S ===================================================
+
+// ============================================================================
+
